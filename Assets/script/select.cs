638448fc99a4010f1f693d17f45fa5e9a54b14a6
YUKI KAMO <k.sakaimasato@icloud.com>
2020-06-02 22:58:38 +0900

    se入らない

---
+++ Assets/script/select.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class select : MonoBehaviour
+{
+    public static string select_swing = "null";
+    public Text explain_text;
+    public GameObject canvas;
+    public static float timelimit = 0.0f;
+    private GameObject meet;
+    private GameObject strong;
+    private GameObject superstrong;
+    public AudioClip choiceBgm;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject meet_res = (GameObject)Resources.Load("meet");
+        GameObject strong_res = (GameObject)Resources.Load("strongswing");
+        GameObject superstrong_res = (GameObject)Resources.Load("superstrong");
+        meet = (GameObject)Instantiate(meet_res, meet_res.transform.position, Quaternion.identity);
+        strong = (GameObject)Instantiate(strong_res, strong_res.transform.position, Quaternion.identity);
+        superstrong = (GameObject)Instantiate(superstrong_res, superstrong_res.transform.position, Quaternion.identity);
+        meet.transform.SetParent(canvas.transform, false);
+        strong.transform.SetParent(canvas.transform, false);
+        superstrong.transform.SetParent(canvas.transform, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Explain()
+    {
+        float uppertime = 10.0f + timelimit;
+        float undertime = 10.0f - timelimit;
+        explain_text.text = "You Select " + select_swing + "\n" 
+        + undertime.ToString() + "から" + uppertime.ToString() + "までにSTOPしましょう！";
+
+    }
+    
+    public void Clickmeet()
+    {
+        GameObject audio_res = (GameObject)Resources.Load("MusicManager");
+        GameObject.Instantiate(audio_res, transform.position, Quaternion.identity).GetComponent<AudioSource>().PlayOneShot(choiceBgm);
+       // audioSource.PlayOneShot(choiceBgm);
+        timelimit = 0.5f;
+        //GameObject.Destroy(meet.gameObject);
+        //GameObject.Destroy(strong.gameObject);
+        //GameObject.Destroy(superstrong.gameObject);
+        select_swing = "meet";
+        //Explain();
+        //Debug.Log("meet");//new!
+        SceneManager.LoadScene("stopwatch");
+
+
+    }
+
+    public void Clickstrong()
+    {
+        GameObject audio_res = (GameObject)Resources.Load("MusicManager");
+        audioSource = GameObject.Instantiate(audio_res, transform.position, Quaternion.identity).GetComponent<AudioSource>();
+        audioSource.PlayOneShot(choiceBgm);
+        timelimit = 0.3f;
+        //GameObject.Destroy(meet.gameObject);
+        //GameObject.Destroy(strong.gameObject);
+        //GameObject.Destroy(superstrong.gameObject);
+        select_swing = "strong";
+        // Explain();     
+        SceneManager.LoadScene("stopwatch");
+    }
+
+    public void Clicksuperstrong()
+    {
+        GameObject audio_res = (GameObject)Resources.Load("MusicManager");
+        audioSource = GameObject.Instantiate(audio_res, transform.position, Quaternion.identity).GetComponent<AudioSource>();
+        audioSource.PlayOneShot(choiceBgm);
+        timelimit = 0.1f;
+        //GameObject.Destroy(meet.gameObject);
+        //GameObject.Destroy(strong.gameObject);
+        //GameObject.Destroy(superstrong.gameObject);
+        select_swing = "superstrong";
+        // Explain();
+        SceneManager.LoadScene("stopwatch");
+    }
+}
---
+++ Assets/script/stopwatch.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class stopwatch : MonoBehaviour
+{
+    public Text timelimit_text;
+    private float time = 0.0f;
+    private float successtime = select.timelimit;
+    //private float successtime = 0.3f;
+    private bool flag = false;
+    private string mors = select.select_swing;
+    //private string mors = "strong";
+    public Text explain;
+    public Text result;
+    public Text botton_text;
+    private bool botton_bool = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mors == "meet")
+        {
+            explain.text = (10 - successtime).ToString() + "から" + (10 + successtime).ToString() + "までにふれば\nヒット！";
+        }else if(mors == "strong")
+        {
+            explain.text = (10 - successtime).ToString() + "から" + (10 + successtime).ToString() + "までにふれば\n２塁打！";
+        }else if(mors == "superstrong")
+        {
+            explain.text = (10 - successtime).ToString() + "から" + (10 + successtime).ToString() + "までにふれば\nホームラン！";
+        }else
+        {
+            explain.text = "エラーです。店員をよんでください。";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(flag == true)
+        {
+            time += Time.deltaTime;
+        }
+        timelimit_text.text = time.ToString();
+    }
+
+    public void stopbotton()
+    {
+        Debug.Log("stop");
+        Debug.Log(botton_bool);
+        if (botton_bool == true){
+            flag = false;
+            if ((10 - successtime) < time && (10 + successtime) > time)
+            {
+                if (mors == "meet")
+                {
+                    result.text = "ヒット！";
+                }
+                else if (mors == "strong")
+                {
+                    result.text = "２塁打！";
+                }
+                else if (mors == "superstrong")
+                {
+                    result.text = "ホームラン！";
+                }
+                else
+                {
+                    result.text = "エラーです。店員をよんでください。";
+                }
+            }
+            else
+            {
+                result.text = "残念、、三振、、、";
+            }
+        }
+        botton_bool = true;
+            
+    } 
+
+    public void startbotton()
+    {
+        Debug.Log("start");
+        Debug.Log(botton_bool);
+        if(botton_bool == false)
+        {
+            flag = true;
+            botton_text.text = "stop";
+        } 
+
+    }
+}
